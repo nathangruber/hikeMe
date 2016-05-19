7417cbe79aae0127fce3e1f5cf4183bad995cd17
nathangruber <grubernp@gmail.com>
2016-05-19 17:28:39 -0500

    sending form

--- index.php
+++ index.php
@@ -17,44 +17,61 @@
         echo "Welcome Back, ";
         echo $_SESSION['name'];
     }
+    
+    
+    if(isset($_POST['search'])){
+	    //We make the call to the API and get the results...
+	    $place = $_POST['search'];
+	    $url = "http://api.openweathermap.org/data/2.5/weather?q=$place&APPID=2bd428fa9cf856303ff450f01f4a97de&units=imperial";
+		$opts = array(
+		        'http' => array (
+		            'method' => 'GET'
+		        )   
+		    );
+		$context = stream_context_create($opts);   //Creates and returns a stream context with any options supplied in options preset.
+		$file = file_get_contents($url, false, $context);  //read the contents of a file into a string
+		$obj = json_decode($file, false); 
+	    
+	    
+	    
+    }
+    
+    
+    
     ?>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
- <?php
- $url = "http://api.openweathermap.org/data/2.5/weather?q=milwaukee&APPID=2bd428fa9cf856303ff450f01f4a97de&units=imperial";
- $opts = array(
-        'http' => array (
-            'method' => 'GET'
-        )   
-    );
- $context = stream_context_create($opts);   //Creates and returns a stream context with any options supplied in options preset.
- $file = file_get_contents($url, false, $context);  //read the contents of a file into a string
- $obj = json_decode($file, false); 
- //show data
- //var_dump($obj);
-
- //description
- //echo $obj['weather'][0]['description'];
- //temperature
- //echo $obj['main']['temp'];
-
-echo "the Lon is: ".$obj->coord->lon;
-echo "<br>";
-echo "the Lon is: ".$obj->coord->lat;
-echo "<br>";
-echo "Weather description: ".$obj->weather->description;
-echo "<br>";
-echo "Farenhei: ".$obj->main->temp;
-
- //print_r($obj);
-
- ?>
-
-
+    
+    
+    <div align="text-center" style="margin-top: 100px;">
+	    <h1>Where do you want to go?</h1>
+		
+		<form action="" method="post">
+			<input type="text" name="search"><br>
+			<input type="submit">
+		</form>
+		
+    </div>
+    
+    
+    
+    <?php
+	    if(isset($_POST['search'])){
+		    
+		    echo "the Lon is: ".$obj->coord->lon;
+			echo "<br>";
+			echo "the Lon is: ".$obj->coord->lat;
+			echo "<br>";
+			echo "Weather description: ".$obj->weather->description;
+			echo "<br>";
+			echo "Farenhei: ".$obj->main->temp;
+	    
+	    
+	    }
+	?>
+    
+    
+    
+    
+    
  
     
 
