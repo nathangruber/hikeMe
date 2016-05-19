7f1d6e9bd2ec7dc12cf25d3acfd419a57f360c8b
nathangruber <grubernp@gmail.com>
2016-05-18 20:15:05 -0500

    testing api call

--- index.php
+++ index.php
@@ -18,16 +18,7 @@
         echo $_SESSION['name'];
     }
     ?>
- 
-
- <!-- http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=2bd428fa9cf856303ff450f01f4a97de  -->
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<?php
+ <?php
  $url = "http://api.openweathermap.org/data/2.5/weather?q=milwaukee&APPID=2bd428fa9cf856303ff450f01f4a97de";
  $opts = array(
         'http' => array (
@@ -36,25 +27,13 @@
     );
  $context = stream_context_create($opts);   //Creates and returns a stream context with any options supplied in options preset.
  $file = file_get_contents($url, false, $context);  //read the contents of a file into a string
- $obj = json_decode($file);    //Takes a JSON encoded string and converts it into a PHP variable.
+ $obj = json_decode($file, $true);    //Takes a JSON encoded string and converts it into a PHP variable.
  print_r($obj);
 
  ?>
 
- <?php
- $url = "https://trailapi-trailapi.p.mashape.com/?q[city_cont]=Milwaukee";
- $opts = array(
-        'http' => array (
-            'method' => 'GET',
-            'header' =>  "Accept: plain/text\r\n". "X-Mashape-Key:F1piInHfmpmsh4V6tDTrAboircv8p1s3rgAjsnO6TthOVVt9rJ "   
-        )   
-    );
- $context = stream_context_create($opts);   //Creates and returns a stream context with any options supplied in options preset.
- $file = file_get_contents($url, false, $context);  //read the contents of a file into a string
- $obj = json_decode($file);    //Takes a JSON encoded string and converts it into a PHP variable.
- print_r($obj);
 
- ?>
+ 
 
     
 
