b35778bd621a0c3a64c6424d22291251b54ac624
nathangruber <grubernp@gmail.com>
2016-05-18 20:50:59 -0500

    testing

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
  $context = stream_context_create($opts);   //Creates and returns a stream context with any options supplied in options preset.
  $file = file_get_contents($url, false, $context);  //read the contents of a file into a string
  $obj = json_decode($file,$true);    //Takes a JSON encoded string and converts it into a PHP variable.
- echo $obj[2]['weather']; // Fetches the first ID
+ echo $obj[3]['main']; // Fetches the first ID
  //echo $obj[2]['main']; // Fetches the first c_name
  
  //print_r($obj);
