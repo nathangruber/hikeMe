336da593c91c2d61f4d753ef97b0223ae0d24d55
nathangruber <grubernp@gmail.com>
2016-05-18 21:44:28 -0500

    test

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
     );
  $context = stream_context_create($opts);   //Creates and returns a stream context with any options supplied in options preset.
  $file = file_get_contents($url, false, $context);  //read the contents of a file into a string
- $obj = json_encode($file, $true); 
+ $obj = json_decode($file, $true); 
  //show data
  //var_dump($obj);
 
