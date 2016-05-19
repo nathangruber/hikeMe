5253fce94dbd85aa3cb7e12c85cb3d7145d2be2f
nathangruber <grubernp@gmail.com>
2016-05-18 20:23:27 -0500

    testing api call

--- index.php
+++ index.php
@@ -33,7 +33,9 @@
     );
  $context = stream_context_create($opts);   //Creates and returns a stream context with any options supplied in options preset.
  $file = file_get_contents($url, false, $context);  //read the contents of a file into a string
- $obj = json_decode($file, $true);    //Takes a JSON encoded string and converts it into a PHP variable.
+ $obj = json_decode($file);    //Takes a JSON encoded string and converts it into a PHP variable.
+ 
+
  print_r($obj);
 
  ?>
