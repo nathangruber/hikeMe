1b7cd8da6f23201cf6256d2ad4239dfdbf1dfab2
nathangruber <grubernp@gmail.com>
2016-05-18 21:13:10 -0500

    edited api syntax

--- index.php
+++ index.php
@@ -34,8 +34,8 @@
  $context = stream_context_create($opts);   //Creates and returns a stream context with any options supplied in options preset.
  $file = file_get_contents($url, false, $context);  //read the contents of a file into a string
  $obj = json_decode($file, $true);    //Takes a JSON encoded string and converts it into a PHP variable.
- foreach($obj as $json){
-   echo $json['key']; // you can access your key value like this if result is array
+ foreach($obj as $file){
+   echo $file['key']; // you can access your key value like this if result is array
  
  //print_r($obj);
 
