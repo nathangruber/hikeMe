9642ebc3c74b1df78e15be7af970ce28fb2e8bea
nathangruber <grubernp@gmail.com>
2016-05-19 17:31:57 -0500

    format html1

--- index.php
+++ index.php
@@ -45,13 +45,14 @@
 	    <h1>Where do you want to go?</h1>
 		
 		<form action="" method="post">
-			<input type="text" name="search"><br>
+			<input class="input" type="text" name="search"><br>
 			<input class="btn btn-default" type="submit">
 		</form>
 		
     </div>
     
     
+   
     
     <?php
 	    if(isset($_POST['search'])){
@@ -71,40 +72,7 @@
     
     
     
-    
-    
- <?php
- $url = "http://api.openweathermap.org/data/2.5/weather?q=milwaukee&APPID=2bd428fa9cf856303ff450f01f4a97de&units=imperial";
- $opts = array(
-        'http' => array (
-            'method' => 'GET'
-        )   
-    );
- $context = stream_context_create($opts);   //Creates and returns a stream context with any options supplied in options preset.
- $file = file_get_contents($url, false, $context);  //read the contents of a file into a string
- $obj = json_decode($file, false); 
- //show data
- //var_dump($obj);
-
- //description
- //echo $obj['weather'][0]['description'];
- //temperature
- //echo $obj['main']['temp'];
-
-echo "the Lon is: ".$obj->coord->lon;
-echo "<br>";
-echo "the Lon is: ".$obj->coord->lat;
-echo "<br>";
-echo "Weather description: ".$obj->weather->description;
-echo "<br>";
-echo "Farenhei: ".$obj->main->temp;
-
- //print_r($obj);
-
- ?>
-
-
- 
+  
     
 
 
