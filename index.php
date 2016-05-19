bf0486f430a555d8c65a62d58fa74f9b7e3304b4
nathangruber <grubernp@gmail.com>
2016-05-18 19:45:21 -0500

    added api call

--- index.php
+++ index.php
@@ -40,6 +40,22 @@
  print_r($obj);
 
  ?>
+
+ <?php
+ $url = "https://trailapi-trailapi.p.mashape.com/?q[city_cont]=Milwaukee";
+ $opts = array(
+        'http' => array (
+            'method' => 'GET',
+            'header' =>  "Accept: plain/text\r\n". "X-Mashape-Key:F1piInHfmpmsh4V6tDTrAboircv8p1s3rgAjsnO6TthOVVt9rJ "   
+        )   
+    );
+ $context = stream_context_create($opts);   //Creates and returns a stream context with any options supplied in options preset.
+ $file = file_get_contents($url, false, $context);  //read the contents of a file into a string
+ $obj = json_decode($file);    //Takes a JSON encoded string and converts it into a PHP variable.
+ print_r($obj);
+
+ ?>
+
     
 
 
