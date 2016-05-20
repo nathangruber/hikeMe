fc5ed9f6c383fe56635d4c480adae71ecff3372b
nathangruber <grubernp@gmail.com>
2016-05-19 21:57:59 -0500

    reverting back to original api call

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 	    //We make the call to the API and get the results...
 	    $place = $_POST['search'];
 	    $loc = urlencode($place);
-	    $url = "http://api.openweathermap.org/data/2.5/forecast?q=milwaukee&APPID=2bd428fa9cf856303ff450f01f4a97de&units=imperial";
+	    $url = "http://api.openweathermap.org/data/2.5/weather?q=$loc&APPID=2bd428fa9cf856303ff450f01f4a97de&units=imperial";
 		$opts = array(
 		        'http' => array (
 		            'method' => 'GET'
@@ -116,10 +116,6 @@
 		<div class="col-xs-12 col-md-4 text-left">
 			<h3>Current Weather</h3>
 			<?php	    
-			    echo "<b>Longitude: </b>".$obj->coord->lon;
-				echo "<br>";
-				echo "<b>Lattitude: </b>".$obj->coord->lat;
-				echo "<br>";
 				echo "<b>Weather description: </b>".$obj->weather[0]->description;
 				echo "<br>";
 				echo "<b>Current Temperature in Farenheit: </b>".$obj->main->temp;
