38b24dad0deb64e15689b130b619797a57b9fa86
nathangruber <grubernp@gmail.com>
2016-05-21 13:22:53 -0500

    editing email form

--- contact.php
+++ contact.php
@@ -1,80 +1,35 @@
+<?php require_once'includes/session.php';?>
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Contact hikeMe</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
-    <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
-   
-  
-     </header> 
+<html lang="en">
+<?php require_once 'includes/header.php';?>
+
+<head>
+    <title>Contact Us</title>
 </head>
 
-  
-
-
-
-  <body>
-<?php
-require_once('navbar.php');
-?>
+<body>
+    <?php require_once'includes/navbar.php';?>
 
     <div class="container">
-        <div class = "row">
-         <div class="col-md-4">
-    
-      
-         <h2></h2>
-<div class = "contactus">       
-<p>Contact Us</p> 
-</div>      
-    
-<form name="contactform" method="post" action="emailform.php">
- 
- 
-  <label for="first_name">First Name *&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
- 
- 
-  <input  type="text" name="first_name" maxlength="45" size="30">
- 
- 
-  <label for="last_name">Last Name *&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
- 
- 
-  <input  type="text" name="last_name" maxlength="45" size="30">
- 
- 
-  <label for="email">Email Address *</label>
- 
- 
-  <input  type="text" name="email" maxlength="45" size="30">
- 
- 
-  <label for="telephone">Phone Number*</label>
- 
-  <input  type="text" name="telephone" maxlength="30" size="30">
- 
-  <label for="comments">Comments *&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
- 
-  <textarea  name="comments" maxlength="1000" cols="30" rows="5"></textarea>
- 
-  <input type="submit" value="Submit">  
+        <div class="row">
+            <div class="col-md-6">
+                <div class="contactus">
+                    <br>
+                    <br>
 
- 
-</form>
-        </div>
-      </div>
-     </div> 
+                    <p>Contact Us</p><br>
 
-<?php
-require_once('footer.php');
-?>
-
-   
-  </body>
+                    <form name="contactform" method="post" action="emailform.php">
+                        <label for="first_name">First Name *&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label> <input type="text" name="first_name" maxlength="45" size="30"> <label for="last_name">Last Name     *&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label> <input type="text" name="last_name" maxlength="45" size="30"> <label for="email">Email Address *&nbsp;</label> <input type="text" name="email" maxlength="45" size="30"> <label for="telephone">Phone Number*</label> <input type="text" name="telephone" maxlength="30" size="30"> <label for="comments">Comments *&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label> 
+                        <textarea name="comments" maxlength="1000" cols="30" rows="5">
+</textarea> <input class="btn btn-default" type="submit" value="Submit">
+                    </form>
+                </div>
+            </div>
+        </div><?php
+                require_once 'includes/footer.php';
+                ?>
+    </div>
+</body>
 </html>
