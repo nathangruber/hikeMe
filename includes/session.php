4e6e755124a3ac8abeb85372055a949d78fc97dc
nathangruber <grubernp@gmail.com>
2016-05-21 03:35:57 -0500

    session problem

--- includes/session.php
+++ includes/session.php
@@ -1,12 +1,13 @@
 <?php
-session_start();
-	
-	$logged = false;
-	if (!empty($_SESSION['id']) && !empty($_SESSION['username'])) {
-		$logged = true;
-		$name = $_SESSION['name'];
-	}
-	
 error_reporting(E_ALL);
-require_once 'database.php';
-require_once 'crud.php';
+session_start();
+$logged = false;
+if (!empty($_SESSION['id']) && !empty($_SESSION['username'])) {
+	$logged = true;
+	$name = $_SESSION['name'];
+	//require_once 'database.php';
+	//require_once 'crud.php';
+}
+	
+
+
