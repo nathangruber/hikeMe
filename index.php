15ed7fe39afdcea171455122113fe3631dee788d
nathangruber <grubernp@gmail.com>
2016-05-21 11:01:36 -0500

    editing api for new data

--- index.php
+++ index.php
@@ -132,9 +132,9 @@ require_once 'includes/crud.php';
 				//echo "<br>";
 				echo "<b>Current Temperature in Farenheit: </b>".$obj->main->temp;
 				echo "<br>";
-				echo "<b>Today's High: </b>".$obj->main->temp_max;
+				echo "<b>Today's High: </b>".$obj->main[0]->temp_max;
 				echo "<br>";
-				echo "<b>Today's Low: </b>".$obj->main->temp_min;
+				echo "<b>Today's Low: </b>".$obj->main[1]->temp_min;
 	    		echo "<br>";
 	    		echo "<b>Wind Speed: </b>".$obj->wind->speed;
 
