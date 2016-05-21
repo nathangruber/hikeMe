09b17ed9663323273999339b30703eb18f0cff57
nathangruber <grubernp@gmail.com>
2016-05-21 12:45:51 -0500

    adding font

--- includes/footer.php
+++ includes/footer.php
@@ -1,22 +1,3 @@
-<!-- <html>
-<head>
-    <title>Footer</title>
-</head>
-
-<body>
-    <footer class="text-left">
-        <p><a href="sitemap.php">Site Map</a></p>
-    </footer>
-
-    <center>
-        <p><small>&copy; 2016 hikeMe</small></p>
-    </center>
-    <script src="assets/js/jquery.min.js" type="text/javascript">
-</script><script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
- <script src="searchFunction.js"></script>
-<script src="assets/js/hidden.js"></script>
-</body>
-</html>  -->
 <html>
 <head>
     <title>Footer</title>
