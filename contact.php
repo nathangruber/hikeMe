3ac3d7a5010c2807168bce0e61074edf288cd27a
nathangruber <grubernp@gmail.com>
2016-05-21 13:29:17 -0500

    testing contact form

--- contact.php
+++ contact.php
@@ -34,7 +34,7 @@
                              <input type="text" name="telephone" maxlength="30" size="30"> 
                             <br>
                              <label for="comments">Comments *&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label> 
-                            <textarea name="comments" maxlength="1000" cols="20" rows="40">
+                            <textarea name="comments" maxlength="1000" cols="40" rows="15">
                             </textarea> <input class="btn btn-default" type="submit" value="Submit">
                         </form>
                 </div>
