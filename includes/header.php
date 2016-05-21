2b4f70ea5082ff56ec66454f93e2ffd36996eda5
nathangruber <grubernp@gmail.com>
2016-05-21 13:37:07 -0500

    added font script

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
 		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 		<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
-		<link href='https://fonts.googleapis.com/css?family=Libre+Baskerville' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
 		<link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
 		<link rel="icon" href="assets/img/favicon.ico" type="image/x-icon">
 		<script type="text/javascript"src="https://maps.googleapis.com/maps/pi/js?client=2bd428fa9cf856303ff450f01f4a97de"></script>
