8ed1f15260a528a78cb4ced7cda09da44a6ce79c
nathangruber <grubernp@gmail.com>
2016-05-21 12:09:32 -0500

    testing footer templates

--- includes/footer.php
+++ includes/footer.php
@@ -22,171 +22,63 @@
 </head>
 
 <body>
-<footer>
-        <div class="splitter"></div>
-
-        <ul>
-<ul>
-
+<footer class="flex-rw">
+  
+  <ul class="footer-list-top">
     <li>
+      <h4 class="footer-list-header">About Pavilion</h4></li>
+    <li><a href='/shop/about-mission' class="generic-anchor footer-list-anchor" itemprop="significantLink">GET TO KNOW US</a></li>
+    <li><a href='/promos.html' class="generic-anchor footer-list-anchor" itemprop="significantLink">PROMOS</a></li>
+    <li><a href='/retailers/new-retailers.html' class="generic-anchor footer-list-anchor" itemprop="significantLink">BECOME A RETAILER</a></li>
 
-        <div class="icon" data-icon="E"></div>
+    <li><a href='/job-openings.html' itemprop="significantLink" class="generic-anchor footer-list-anchor">JOB OPENINGS</a></li>
 
-        <div class="text">
-
-            <h4>About</h4>
-
-            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tristique justo eu sollicitudin pretium. Nam scelerisque arcu at dui porttitor, non viverra sapien pretium. Nunc nec dignissim nunc. Sed eget est purus. Sed convallis, metus in dictum feugiat, odio orci rhoncus metus. <a href="#">Read more</a></div>
-
-        </div>
-
-    </li>
-
+    <li><a href='/shop/about-show-schedule' class="generic-anchor footer-list-anchor" itemprop="significantLink">EVENTS</a></li>
+  </ul>
+  <ul class="footer-list-top">
     <li>
+      <h4 class="footer-list-header">The Gift Selection</h4></li>
 
-        <div class="icon" data-icon="a"></div>
 
-        <div class="text">
-
-            <h4>Archive</h4>
-
-            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tristique justo eu sollicitudin pretium. Nam scelerisque arcu at dui porttitor, non viverra sapien pretium. Nunc nec dignissim nunc. Sed eget est purus. Sed convallis, metus in dictum feugiat, odio orci rhoncus metus. <a href="#">Read more</a></div>
-
-        </div>
-
-    </li>
-
-    <li>
-
-        <div class="icon" data-icon="s"></div>
-
-        <div class="text">
-
-            <h4>Cloud</h4>
-
-            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tristique justo eu sollicitudin pretium. Nam scelerisque arcu at dui porttitor, non viverra sapien pretium. Nunc nec dignissim nunc. Sed eget est purus. Sed convallis, metus in dictum feugiat, odio orci rhoncus metus. <a href="#">Read more</a></div>
-
-        </div>
-
-    </li>
-
-</ul>
-
-            <!-- three footer columns are here -->
-
-        </ul>
-
+    <li><a href='/Angels/cat/id/70' class="generic-anchor footer-list-anchor">ANGEL FIGURINES</a></li>
+    <li><a href='/Home-Decor/cat/id/64' class="generic-anchor footer-list-anchor">HOME DECOR</a></li>
+    <li><a href='/Mugs/cat/id/32' class="generic-anchor footer-list-anchor">MUGS</a></li>
+    <li><a href='/Pet-Lover/cat/id/108' class="generic-anchor footer-list-anchor">PET LOVER</a></li>
+    <li><a href='/Ladies-Accessories/cat/id/117' class="generic-anchor footer-list-anchor" target="_blank">HANDBAGS & JEWELRY</a></li>
+  </ul>
+  <ul class="footer-list-top">
+    <li id='help'>
+      <h4 class="footer-list-header">Please Help Me</h4></li>
+    <li><a href='/shop/about-contact' class="generic-anchor footer-list-anchor" itemprop="significantLink">CONTACT</a></li>
+    <li><a href='/faq.html' class="generic-anchor footer-list-anchor" itemprop="significantLink">FAQ</a></li>
+    <li id='find-a-store'><a href='/shop/store-locator' class="generic-anchor footer-list-anchor" itemprop="significantLink">STORE LOCATOR</a></li>
+    <li id='user-registration'><a href='/shop/user-registration?URL=' class="generic-anchor footer-list-anchor" itemprop="significantLink">NEW USERS</a></li>
+    <li id='order-tracking'><a href='/shop/order-status' itemprop="significantLink" class="generic-anchor footer-list-anchor">ORDER STATUS</a></li>
+  </ul>
+  <section class="footer-social-section flex-rw">
+      <span class="footer-social-overlap footer-social-connect">
+      CONNECT <span class="footer-social-small">with</span> US
+      </span>
+      <span class="footer-social-overlap footer-social-icons-wrapper">
+      <a href="http://www.pinterest.com/paviliongift/" class="generic-anchor" target="_blank" title="Pinterest" itemprop="significantLink"><i class="fa fa-pinterest"></i></a>
+      <a href="https://www.facebook.com/paviliongift" class="generic-anchor" target="_blank" title="Facebook" itemprop="significantLink"><i class="fa fa-facebook"></i></a>
+      <a href="https://twitter.com/PavilionGiftCo" class="generic-anchor" target="_blank" title="Twitter" itemprop="significantLink"><i class="fa fa-twitter"></i></a>
+      <a href="http://instagram.com/paviliongiftcompany" class="generic-anchor" target="_blank" title="Instagram" itemprop="significantLink"><i class="fa fa-instagram"></i></a>
+      <a href="https://www.youtube.com/channel/UCYgUODvd0qXbu_LkUWpTVEg" class="generic-anchor" target="_blank" title="Youtube" itemprop="significantLink"><i class="fa fa-youtube"></i></a>
+      <a href="https://plus.google.com/+Paviliongift/posts" class="generic-anchor" target="_blank" title="Google Plus" itemprop="significantLink"><i class="fa fa-google-plus"></i></a>
+      </span>
+  </section>
+  <section class="footer-bottom-section flex-rw">
+<div class="footer-bottom-wrapper">   
+<i class="fa fa-copyright" role="copyright">
  
-
-        <div class="bar">
-
-            <div class="bar-wrap">
-
-                <ul class="links"> <!-- footer menu -->
-
-                    <li><a href="#">Home</a></li>
-
-                    <li><a href="#">License</a></li>
-
-                    <li><a href="#">Contact Us</a></li>
-
-                    <li><a href="#">Advertise</a></li>
-
-                    <li><a href="#">About</a></li>
-                </ul>
-
- 
-
-                <div class="social">
-<div class="bar">
-
-    <div class="bar-wrap">
-
-        <ul class="links">
-
-            <li><a href="#">Home</a></li>
-
-            <li><a href="#">License</a></li>
-
-            <li><a href="#">Contact Us</a></li>
-
-            <li><a href="#">Advertise</a></li>
-
-            <li><a href="#">About</a></li>
-
-        </ul>
-
- 
-
-        <div class="social">
-
-            <a href="#" class="fb">
-
-                <span data-icon="f" class="icon"></span>
-
-                <span class="info">
-                    <span class="follow">Become a fan Facebook</span>
-
-                    <span class="num">9,999</span>
-
-                </span>
-
-            </a>
-
- 
-
-            <a href="#" class="tw">
-
-                <span data-icon="T" class="icon"></span>
-
-                <span class="info">
-
-                    <span class="follow">Follow us Twitter</span>
-
-                    <span class="num">9,999</span>
-
-                </span>
-
-            </a>
-
- 
-
-            <a href="#" class="rss">
-
-                <span data-icon="R" class="icon"></span>
-
-                <span class="info">
-                    <span class="follow">Subscribe RSS</span>
-
-                    <span class="num">9,999</span>
-
-                </span>
-
-            </a>
-
-        </div>
-
-        <div class="clear"></div>
-
-        <div class="copyright">&copy;  2014 All Rights Reserved</div>
-
+</i> 2015 Pavilion in <address class="footer-address" role="company address">Bergen, NY</address><span class="footer-bottom-rights"> - All Rights Reserved - </span>
     </div>
-
-</div>
-
-                    <!-- social icons are here -->
-
-                </div>
-
-                <div class="clear"></div>
-
-                <div class="copyright">&copy;  2014 All Rights Reserved</div>
-
-            </div>
-
-        </div>
-
-    </footer>
+    <div class="footer-bottom-wrapper">
+    <a href="/terms-of-use.html" class="generic-anchor" rel="nofollow">Terms</a> | <a href="/privacy-policy.html" class="generic-anchor" rel="nofollow">Privacy</a>
+      </div>
+  </section>
+</footer>
 
 </body>
 </html> 
