a6039a906d92aa62ac20c88c03fe4f6488e4bc1d
nathangruber <grubernp@gmail.com>
2016-05-22 13:14:00 -0500

    editing tags

--- cover.php
+++ cover.php
@@ -11,8 +11,10 @@
 <div class="image">
  
 <a href="http://ec2-54-213-132-61.us-west-2.compute.amazonaws.com/hikeMe/index.php"><img src="assets/img/away-1356948_1920.jpg" alt="Beautiful Hiking Path"/>
-
-        <center><h2 style="color:#FFFFFF;">hikeME<br /><br><br><br><br><small>click to enter</small></h2></center>
+<style>
+h2 {color:#FFFFFF;}
+</style>
+        <center><h2>hikeME<br /><br><br><br><br><small>click to enter</small></h2></center>
 
 </div>
 
