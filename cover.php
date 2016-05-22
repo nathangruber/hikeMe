0d43d4aac6c4fef3fc5042b09d4bee50e61a8489
nathangruber <grubernp@gmail.com>
2016-05-22 12:27:53 -0500

    edited tags
    too many h2’s

--- cover.php
+++ cover.php
@@ -12,7 +12,7 @@
  
 <a href="http://ec2-54-213-132-61.us-west-2.compute.amazonaws.com/hikeMe/index.php"><img src="assets/img/away-1356948_1920.jpg" alt="Beautiful Hiking Path"/>
 
-        <center><h2>hikeME<br /><small>click anywhere to plan or revisit your Hikes</small></h2></center>
+        <center><p>hikeME<br /><small>click anywhere to plan or revisit your Hikes</small></p></center>
 
 </div>
 
