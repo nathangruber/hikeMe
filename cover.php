39c51c5e3a000f4efd17de4526a355df46bd983b
nathangruber <grubernp@gmail.com>
2016-05-22 18:13:45 -0500

    styling front page

--- cover.php
+++ cover.php
@@ -7,12 +7,12 @@
 </head>
 
 <body>
-
+<p>you may wander, but you'll never be lost. Plan and remember your hikes</p>
 <div class="image">
  
 <a href="http://ec2-54-213-132-61.us-west-2.compute.amazonaws.com/hikeMe/index.php"><img src="assets/img/away-1356948_1920.jpg" alt="Beautiful Hiking Path"/>
 
-        <center><h2>hikeME</center><br /><br><br><br><small>you may wander, but you'll never be lost. plan and remember your hikes.</small></h2>
+        <center><h2>hikeME</h2></center>
 
 </div>
 
