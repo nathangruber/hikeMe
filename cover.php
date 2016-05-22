d8172739c32d38cbd94339eaa143365a381132e5
nathangruber <grubernp@gmail.com>
2016-05-22 13:01:52 -0500

    adjusting font size

--- cover.php
+++ cover.php
@@ -12,26 +12,13 @@
  
 <a href="http://ec2-54-213-132-61.us-west-2.compute.amazonaws.com/hikeMe/index.php"><img src="assets/img/away-1356948_1920.jpg" alt="Beautiful Hiking Path"/>
 
-        <center><h2>hikeME<br /><small>click anywhere to plan or revisit your Hikes</small></h2></center>
-
+<center><h2>hikeME</h2></center>
+<br>
+<br>
+<br>
+<br>
+<p>click to enter</p>
 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <?php require_once 'includes/footer.php'; ?>
 </body>
 </html>
