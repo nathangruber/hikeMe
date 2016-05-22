d59032dab173393a24e49fc57dcec84049ae1df9
nathangruber <grubernp@gmail.com>
2016-05-22 13:04:45 -0500

    testing styles

--- cover.php
+++ cover.php
@@ -12,10 +12,26 @@
  
 <a href="http://ec2-54-213-132-61.us-west-2.compute.amazonaws.com/hikeMe/index.php"><img src="assets/img/away-1356948_1920.jpg" alt="Beautiful Hiking Path"/>
 
-<center><h2>hikeME</h2></center>
-<br>
-<p>click to enter</p>
+        <center><h2>hikeME<br /><small>click to enter</small></h2></center>
+
 </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <?php require_once 'includes/footer.php'; ?>
 </body>
 </html>
