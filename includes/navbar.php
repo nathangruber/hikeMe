1da9852d63f889f9272c3ce2d52e8f09953396fd
nathangruber <grubernp@gmail.com>
2016-05-22 18:03:31 -0500

    editing link

--- includes/navbar.php
+++ includes/navbar.php
@@ -10,7 +10,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-         <a class="navbar-brand" href="index.php" id="black"><img src="assets/img/bbs.png" height="26" width="26"></a>
+         <a class="navbar-brand" href="cover.php"><img src="assets/img/bbs.png" height="26" width="26"></a>
      </div>
 
 
