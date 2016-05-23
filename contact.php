27dd7276a3d2d6b53000fce4c7eb0c459e49444f
nathangruber <grubernp@gmail.com>
2016-05-22 21:01:30 -0500

    changing contact form

--- contact.php
+++ contact.php
@@ -20,7 +20,7 @@
 
                     <p>Contact Us</p><br>
 
-                         <form name="contactform" method="post" action="emailform.php">
+                     <!--    <form name="contactform" method="post" action="emailform.php">
                             <label for="first_name">First Name *&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label> 
                             <input type="text" name="first_name" maxlength="45" size="30"> 
                             <br>
@@ -40,7 +40,35 @@
                 </div>
             </div>
         </div>
+    </div>  -->
+<form class="form-horizontal">
+  <div class="form-group">
+    <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
     </div>
+  </div>
+  <div class="form-group">
+    <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <div class="checkbox">
+        <label>
+          <input type="checkbox"> Remember me
+        </label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <button type="submit" class="btn btn-default">Sign in</button>
+    </div>
+  </div>
+</form>
     <?php require_once 'includes/footer.php';?>
 </body>
 </html>
