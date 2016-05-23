0f85d9064f8d29ad3747615e2c33b9ab8ec2c17d
nathangruber <grubernp@gmail.com>
2016-05-23 16:29:30 -0500

    adding bootstrap cdn

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+		<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+        <link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
 		<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
 		<link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
 		<link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
