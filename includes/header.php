ce62f0d851c2cecfdeb9a92deaad71fb89377a7d
nathangruber <grubernp@gmail.com>
2016-05-23 16:55:07 -0500

    navbar troubleshoot

--- includes/header.php
+++ includes/header.php
@@ -2,9 +2,13 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-		<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+		<meta name="author" content="Nathan Gruber">
+		 <meta name="description" content="hikeMe">
+
+		 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" type="text/css">
+         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" type="text/css">
+		
+		<link rel="stylesheet" type="text/styles" href="assets/css/styles.css">
 		<link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
 		<link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
 		<link rel="icon" href="assets/img/favicon.ico" type="image/x-icon">
@@ -22,3 +26,7 @@
 		<script type="text/javascript">
 </script>
 </head>
+
+ 
+   
+    
