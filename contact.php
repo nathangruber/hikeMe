e7af62928df131afc428c64ac54102209b4ee75f
nathangruber <grubernp@gmail.com>
2016-05-22 21:12:24 -0500

    editing contact form

--- contact.php
+++ contact.php
@@ -23,19 +23,19 @@
                       <div class="form-group">
                         <label for="inputEmail3" class="col-sm-2 control-label">First Name</label>
                         <div class="col-sm-10">
-                          <input type="email" class="form-control" id="inputEmail3" placeholder="First Name">
+                          <input type="text" class="form-control" id="inputEmail3" placeholder="First Name">
                         </div>
                       </div>
                       <div class="form-group">
                         <label for="inputEmail3" class="col-sm-2 control-label">Last Name</label>
                         <div class="col-sm-10">
-                          <input type="email" class="form-control" id="inputEmail3" placeholder="Last Name">
+                          <input type="text" class="form-control" id="inputEmail3" placeholder="Last Name">
                         </div>
                       </div>
                       <div class="form-group">
                         <label for="inputPassword3" class="col-sm-2 control-label">Email Address</label>
                         <div class="col-sm-10">
-                          <input type="password" class="form-control" id="inputPassword3" placeholder="Email Address">
+                          <input type="email" class="form-control" id="inputPassword3" placeholder="Email Address">
                         </div>
                       </div>
                       <div class="form-group">
