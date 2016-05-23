620f904f423fcaa5c89b69fbc51da1cc4db3aad4
nathangruber <grubernp@gmail.com>
2016-05-22 23:44:48 -0500

    moved cover text

--- cover.php
+++ cover.php
@@ -2,36 +2,15 @@
 <html lang="en">
 <?php require_once 'includes/header.php';?>
 <head>
-    <title>hikeMe</title>
-		<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
-		<link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+	<title>hikeMe</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+	<link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
 </head>
-
 <body>
-<center><p>you may wander, but you'll never be lost. plan and remember your hikes</p></center>
-<div class="image">
- 
-<a href="http://ec2-54-213-132-61.us-west-2.compute.amazonaws.com/hikeMe/index.php"><img src="assets/img/away-1356948_1920.jpg" alt="Beautiful Hiking Path"/>
-
-        <center><h2>hikeME</h2></center>
-
+	<center><p>you may wander, but you'll never be lost. plan and remember your hikes</p></center>
+	<div class="image">
+	 <a href="http://ec2-54-213-132-61.us-west-2.compute.amazonaws.com/hikeMe/index.php"><img src="assets/img/away-1356948_1920.jpg" alt="Beautiful Hiking Path"/>
+	<center><h2>hikeME</h2></center>
 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 </body>
 </html>
