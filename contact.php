6bfd3944c6ac29af7310b123b553a900cd906625
nathangruber <grubernp@gmail.com>
2016-05-22 21:10:33 -0500

    removed old form

--- contact.php
+++ contact.php
@@ -19,59 +19,42 @@
                     <br>
 
                     <p>Contact Us</p><br>
-
-                            <form name="contactform" method="post" action="emailform.php">
-                            <label for="first_name">First Name *&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label> 
-                            <input type="text" name="first_name" maxlength="45" size="30"> 
-                            <br>
-                            <label for="last_name">Last Name     *&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label> 
-                            <input type="text" name="last_name" maxlength="45" size="30"> 
-                            <br>
-                            <label for="email">Email Address *&nbsp;</label>
-                             <input type="text" name="email" maxlength="45" size="30"> 
-                             <br>
-                             <label for="telephone">Phone Number*&nbsp;</label> 
-                             <input type="text" name="telephone" maxlength="30" size="30"> 
-                            <br>
-                             <label for="comments">Comments *&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label> 
-                            <textarea name="comments" maxlength="1000" cols="40" rows="15">
-                            </textarea> <input class="btn btn-default" type="submit" value="Submit">
-                        </form>
+                     <form class="form-horizontal">
+                      <div class="form-group">
+                        <label for="inputEmail3" class="col-sm-2 control-label">First Name</label>
+                        <div class="col-sm-10">
+                          <input type="email" class="form-control" id="inputEmail3" placeholder="First Name">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label for="inputEmail3" class="col-sm-2 control-label">Last Name</label>
+                        <div class="col-sm-10">
+                          <input type="email" class="form-control" id="inputEmail3" placeholder="Last Name">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label for="inputPassword3" class="col-sm-2 control-label">Email Address</label>
+                        <div class="col-sm-10">
+                          <input type="password" class="form-control" id="inputPassword3" placeholder="Email Address">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label for="inputPassword3" class="col-sm-2 control-label">Comments</label>
+                        <div class="col-sm-10">
+                          <textarea class="form-control" rows="3"></textarea>
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                          <button type="submit" class="btn btn-default">Submit</button>
+                        </div>
+                      </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div> 
-<form class="form-horizontal">
-  <div class="form-group">
-    <label for="inputEmail3" class="col-sm-2 control-label">First Name</label>
-    <div class="col-sm-10">
-      <input type="email" class="form-control" id="inputEmail3" placeholder="First Name">
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="inputEmail3" class="col-sm-2 control-label">Last Name</label>
-    <div class="col-sm-10">
-      <input type="email" class="form-control" id="inputEmail3" placeholder="Last Name">
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="inputPassword3" class="col-sm-2 control-label">Email Address</label>
-    <div class="col-sm-10">
-      <input type="password" class="form-control" id="inputPassword3" placeholder="Email Address">
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="inputPassword3" class="col-sm-2 control-label">Comments</label>
-    <div class="col-sm-10">
-      <textarea class="form-control" rows="3"></textarea>
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="col-sm-offset-2 col-sm-10">
-      <button type="submit" class="btn btn-default">Submit</button>
-    </div>
-  </div>
-</form>
+
     <?php require_once 'includes/footer.php';?>
 </body>
 </html>
