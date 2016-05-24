322fc69c85eaf2ced0c4cce61ff48f1e26064236
nathangruber <grubernp@gmail.com>
2016-05-23 22:55:54 -0500

    edited nav

--- includes/navbar.php
+++ includes/navbar.php
@@ -19,7 +19,7 @@
 
     <ul class="nav navbar-nav">
 
-	    <li><a href="index.php">hikeMe</a></li>
+	    <li><a href="index.php">Home</a></li>
 	    <li><a href="contact.php">Contact Us</a></li>
 	    <li><a href="myhikes.php">My Hikes</a></li>
     </ul>
