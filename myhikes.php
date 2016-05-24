03fed6650ff369494632724d72967dede18499b0
nathangruber <grubernp@gmail.com>
2016-05-23 22:36:58 -0500

    added break

--- myhikes.php
+++ myhikes.php
@@ -124,7 +124,7 @@ $favorite_plans = $plan->getMyFavorites($_SESSION['id']);
 
 				<div class="fb-share-button right-button" data-href="http://ec2-54-213-132-61.us-west-2.compute.amazonaws.com/hikeMe/cover.php" data-layout="button" data-mobile-iframe="true"></div></div>
 				
-
+<br>
 				<?php
 				for($i=0;$i<count($favorite_plans);$i++){
 					?>
