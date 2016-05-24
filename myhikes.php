b429e2f9b3982ea461a1b2f5dc187d4354a14997
nathangruber <grubernp@gmail.com>
2016-05-23 21:37:28 -0500

    added comma

--- myhikes.php
+++ myhikes.php
@@ -97,7 +97,7 @@ $favorite_plans = $plan->getMyFavorites($_SESSION['id']);
     <?php } ?>
     
     <div class="text-center" style="margin-top: 100px;">
-	    <h1>Welcome Back <?php echo $name; ?></h1>
+	    <h1>Welcome Back, <?php echo $name; ?></h1>
 		
 		<div class="row">
 			<div class="col-xs-12 col-md-6 col-md-offset-3">
