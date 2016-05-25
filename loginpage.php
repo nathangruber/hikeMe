0f62285ddaacc2c452ce3eac82f4cb3aadb63aa4
nathangruber <grubernp@gmail.com>
2016-05-24 20:13:43 -0500

    corrected center

--- loginpage.php
+++ loginpage.php
@@ -42,7 +42,7 @@
 					</form>
 		        
 		        <div>
-		            <center<p>No Account? Register below</p></center>
+		            <center><p>No Account? Register below</p></center>
 		            <center><a href="register.php" class="btn btn-default">Register</a></center>
 		            <br>
 		        </div>
