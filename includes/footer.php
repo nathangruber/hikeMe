ed631c06703f645bc841404e978ababbbe7a6297
nathangruber <grubernp@gmail.com>
2016-05-25 17:26:06 -0500

    tabbed code

--- includes/footer.php
+++ includes/footer.php
@@ -4,22 +4,18 @@
 </head>
 
 <body>
-<div class="content">hikeMe</div>
-<div class="footer">
-  <div class="social">&#62220;</div>
-  <div class="social">&#62217;</div>
-  <div class="social">&#62223;</div>
-  <div class="social">&#62226;</div>
-</div>
- <center>
-        <p><small>&copy; 2016 hikeMe</small></p>
-    </center>
-    <footer class="text-left">
+  <div class="content">hikeMe</div>
+  <div class="footer">
+    <div class="social">&#62220;</div>
+    <div class="social">&#62217;</div>
+    <div class="social">&#62223;</div>
+    <div class="social">&#62226;</div>
+  </div>
+        <center><p><small>&copy; 2016 hikeMe</small></p></center>
+<footer class="text-left">
         <p><a href="sitemap.php">Site Map</a></p>
-    </footer>
-     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js" type="text/javascript">
-</script><!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" type="text/javascript">
-</script>
+ </footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js" type="text/javascript"></script><!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" type="text/javascript"></script>
 </body>
 </html> 
