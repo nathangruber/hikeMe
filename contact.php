b8491a331c8e187488601bbf0aa858e7fca9f234
nathangruber <grubernp@gmail.com>
2016-05-26 17:39:35 -0500

    removed page break

--- contact.php
+++ contact.php
@@ -24,7 +24,6 @@
                       <div class="form-group">
                         <label for="first_name" class="col-sm-2 control-label">First Name</label>
                         <div class="col-sm-10">
-                        <br>
                         <input type="text" class="form-control" name="first_name" placeholder="First Name">
                         </div>
                       </div>
