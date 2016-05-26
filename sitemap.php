3f48dda6ac91cdf281a586d43862bb8eab21041a
nathangruber <grubernp@gmail.com>
2016-05-25 22:47:24 -0500

    cleaned sitemap

--- sitemap.php
+++ sitemap.php
@@ -9,48 +9,18 @@
                 <h1>Site Map</h1>
                 <p><a href="cover.php">Cover Page</a></p>
 
-                <ul>
-                    <li>Enter hikeMe</li>
-                </ul>
-                
                 <p><a href="index.php">Home Page</a></p>
-
-                <ul>
-                    <li>Search for your next hiking adventure</li>
-                </ul>
 
                 <p><a href="myHikes.php">My Hikes</a></p>
 
-                <ul>
-                    <li>Search Trails</li>
-
-                    <li>Plan your Hikes</li>
-
-                    <li>Rememeber your Hikes</li>
-                </ul>
-
                 <p><a href="contact.php">Contact hikeMe</a></p>
-
-                <ul>
-                    <li>Contact Us</li>
-                </ul>
 
                 <p><a href="register.php">Register</a></p>
 
-                <ul>
-                    <li>Register</li>
-                </ul>
-
                 <p><a href="loginpage.php">Login</a></p>
 
-                <ul>
-                    <li>Login</li>
-                </ul>
-
-               
             </div>
         </div>
     </div>
-<?php require_once 'includes/footer.php';?>
 </body>
 </html>
