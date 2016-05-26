fcdc211a8256280cc9911fa4a020c106826db0f3
nathangruber <grubernp@gmail.com>
2016-05-26 17:18:36 -0500

    adding text to img

--- cover.php
+++ cover.php
@@ -13,7 +13,10 @@
 	<div class="image">
 		<a href="http://ec2-54-213-132-61.us-west-2.compute.amazonaws.com/hikeMe/index.php"><img src="assets/img/away-1356948_1920.jpg" alt="Beautiful Hiking Path"/>
 		<center><h2>hikeME</h2></center>
-		<p>This is some demonstration text</p>
+		<div class="module cap">
+  <p>Skyscraper are really neat and stuff because they are tall.</p>
+		</div>
+
 
 	</div>
 </body>
