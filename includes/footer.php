9697fb8e34337ef0e6933ff0e266cef2e959cb22
nathangruber <grubernp@gmail.com>
2016-05-25 22:30:57 -0500

    sticky footer - testing

--- includes/footer.php
+++ includes/footer.php
@@ -6,6 +6,9 @@
     <div class="social">&#62223;</div>
     <div class="social">&#62226;</div>
   </div>
-  <center><p><small>&copy; 2016 hikeMe. Created by Nathan Gruber. All rights reserved. <a href="sitemap.php">Site Map</a></small></p></center>
+  <center><p><small>&copy; 2016 hikeMe. All rights reserved. <a href="sitemap.php">Site Map</a></small></p></center>
+  <footer class="text-left">
+    <p><a href="sitemap.php">Site Map</a></p>
+  </footer>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js" type="text/javascript"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" type="text/javascript"></script>
