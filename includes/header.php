052b13c8c6b107d83be61d35bdc472732475477f
nathangruber <grubernp@gmail.com>
2016-05-25 22:33:55 -0500

    removed extra assets

--- includes/header.php
+++ includes/header.php
@@ -7,15 +7,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" type="text/css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" type="text/css">
     <link rel="stylesheet" type="text/styles" href="assets/css/styles.css">
-    <link rel="stylesheet" type="text/styles" href="assets/css/styles.css">
 	<link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
     <link rel="icon" href="assets/img/favicon.ico" type="image/x-icon">
-   	<script src="assets/js/bootstrap.min.js"></script>
-	<script src="assets/js/jquery-1.12.0.min.js"></script>
-   	<script src="assets/js/jquery.min.js"></script>
-		<title>hikeMe</title>
+	<title>hikeMe</title>
 </head>
-
- 
-   
-    
