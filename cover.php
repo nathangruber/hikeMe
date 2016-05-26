c701e9dce10a302c015cce877b9ffa6493eb878a
nathangruber <grubernp@gmail.com>
2016-05-26 00:04:47 -0500

    testing cover text

--- cover.php
+++ cover.php
@@ -8,9 +8,19 @@
 <body>
 		<title>hikeMe</title>
 
-	<center><p>“Now I see the secret of making the best person, it is to grow in the open air and to eat and sleep with the earth.”
+<TABLE BORDER="0" cellpadding="0" CELLSPACING="0">
+<TR>
+
+<TD WIDTH="221" HEIGHT="300" BACKGROUND="away-1356948_1920.jpg" VALIGN="bottom">
+
+<FONT SIZE="+1" COLOR="black"></FONT>“Now I see the secret of making the best person, it is to grow in the open air and to eat and sleep with the earth.”
+– Walt Whitman</TD>
+
+</TR>
+</TABLE>
+	<!--<center><p>“Now I see the secret of making the best person, it is to grow in the open air and to eat and sleep with the earth.”
 – Walt Whitman</p></center>
-	<div class="image">
+	<div class="image"> -->
 		<a href="http://ec2-54-213-132-61.us-west-2.compute.amazonaws.com/hikeMe/index.php"><img src="assets/img/away-1356948_1920.jpg" alt="Beautiful Hiking Path"/>
 		<center><h2>hikeME</h2></center>
 	</div>
