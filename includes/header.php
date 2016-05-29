51b76715b6b707dd2fd06d56414e86ee90814d61
nathangruber <grubernp@gmail.com>
2016-05-29 12:12:26 -0500

    added twitter meta tags

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="Nathan Gruber">
     <meta name="description" content="hikeMe">
+    <meta property="og:http://ec2-54-213-132-61.us-west-2.compute.amazonaws.com/hikeMe/cover.php"content="http://www.your-domain.com/your-page.html" />
+    <meta property="og:type"          content="website" />
+    <meta property="og:title"         content="hikeMe" />
+    <meta property="og:description"   content="Search, Plan, Remember, and Share your hikes" />
+    <meta property="og:image"         content="http://www.your-domain.com/path/image.jpg" />
     <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" type="text/css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" type="text/css">
