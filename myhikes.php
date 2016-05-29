fa639a6d6b6fa912d49418d716aff69cfc7b4b7e
nathangruber <grubernp@gmail.com>
2016-05-29 11:34:53 -0500

    removed break

--- myhikes.php
+++ myhikes.php
@@ -128,11 +128,9 @@ $favorite_plans = $plan->getMyFavorites($_SESSION['id']);
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
                 </script>
 				<div class="right">
-
 				<div class="fb-share-button right-button" data-href="http://ec2-54-213-132-61.us-west-2.compute.amazonaws.com/hikeMe/cover.php" data-layout="button" data-mobile-iframe="true"></div></div>
 				<div<a data-pin-do="buttonPin" data-pin-count="above" data-pin-round="true" href="https://www.pinterest.com/pin/create/button/?url=http%3A%2F%2Fec2-54-213-132-61.us-west-2.compute.amazonaws.com%2FhikeMe%2Fcover.php&media=https%3A%2F%2Ffarm8.staticflickr.com%2F7027%2F6851755809_df5b2051c9_z.jpg&description=Next%20stop%3A%20Pinterest"><img src="//assets.pinterest.com/images/pidgets/pinit_fg_en_round_red_16.png" /></a></div>
 
-<br>
 			<?php
 				for($i=0;$i<count($favorite_plans);$i++){
 					?>
