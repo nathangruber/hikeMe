744a6f216272f2e0dee454d6f2941c12707c4215
nathangruber <grubernp@gmail.com>
2016-05-29 11:25:35 -0500

    edited pinterest button

--- myhikes.php
+++ myhikes.php
@@ -92,7 +92,6 @@ $favorite_plans = $plan->getMyFavorites($_SESSION['id']);
 	  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
 	  fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));</script>
-    <a data-pin-do="buttonPin" data-pin-count="above" data-pin-round="true" href="https://www.pinterest.com/pin/create/button/?url=http%3A%2F%2Fec2-54-213-132-61.us-west-2.compute.amazonaws.com%2FhikeMe%2Fcover.php&media=https%3A%2F%2Ffarm8.staticflickr.com%2F7027%2F6851755809_df5b2051c9_z.jpg&description=Next%20stop%3A%20Pinterest"><img src="//assets.pinterest.com/images/pidgets/pinit_fg_en_round_red_16.png" /></a>
 	<title>hikeMe</title>
      <?php
 	 require_once'includes/navbar.php';?>
@@ -134,7 +133,8 @@ $favorite_plans = $plan->getMyFavorites($_SESSION['id']);
 				<div class="fb-share-button right-button" data-href="http://ec2-54-213-132-61.us-west-2.compute.amazonaws.com/hikeMe/cover.php" data-layout="button" data-mobile-iframe="true"></div></div>
 <br>				
 <br>
-				<?php
+				<a data-pin-do="buttonPin" data-pin-count="above" data-pin-round="true" href="https://www.pinterest.com/pin/create/button/?url=http%3A%2F%2Fec2-54-213-132-61.us-west-2.compute.amazonaws.com%2FhikeMe%2Fcover.php&media=https%3A%2F%2Ffarm8.staticflickr.com%2F7027%2F6851755809_df5b2051c9_z.jpg&description=Next%20stop%3A%20Pinterest"><img src="//assets.pinterest.com/images/pidgets/pinit_fg_en_round_red_16.png" /></a>
+			<?php
 				for($i=0;$i<count($favorite_plans);$i++){
 					?>
 					<div class="row">
