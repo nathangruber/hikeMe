6f345b892f77d580fb7678cc601877fdc47b48d6
nathangruber <grubernp@gmail.com>
2016-05-29 22:25:45 -0500

    added small

--- cover.php
+++ cover.php
@@ -12,7 +12,7 @@
 – Walt Whitman</p></center> -->
 	<div class="image">
 		<a href="http://ec2-54-213-132-61.us-west-2.compute.amazonaws.com/hikeMe/index.php"><img src="assets/img/away-1356948_1920.jpg" alt="Beautiful Hiking Path"/>
-		<center><h2>hikeME<br>search, plan, remember, and share your hikes</h2></center>
+		<center><h2>hikeME<br><small>search, plan, remember, and share your hikes</small></h2></center>
 		
 	</div>
 </body>
