af9e5d767a0979c7ad7e9bd502305105f7be101b
nathangruber <grubernp@gmail.com>
2016-05-29 22:25:15 -0500

    adding text to cover

--- cover.php
+++ cover.php
@@ -6,13 +6,13 @@
 	<link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
 </head>
 <body>
-		<title>hikeMe search, plan, remember, and share your hikes</title>
+		<title>hikeMe</title>
 
 <!--	<center><p>“Now I see the secret of making the best person, it is to grow in the open air and to eat and sleep with the earth.”
 – Walt Whitman</p></center> -->
 	<div class="image">
 		<a href="http://ec2-54-213-132-61.us-west-2.compute.amazonaws.com/hikeMe/index.php"><img src="assets/img/away-1356948_1920.jpg" alt="Beautiful Hiking Path"/>
-		<center><h2>hikeME</h2></center>
+		<center><h2>hikeME<br>search, plan, remember, and share your hikes</h2></center>
 		
 	</div>
 </body>
