2d462e575a88699d29d274ddb03235198ccf208b
nathangruber <grubernp@gmail.com>
2016-05-29 19:04:58 -0500

    removed banner
    text edit

--- planhike.php
+++ planhike.php
@@ -57,12 +57,11 @@ $obj = json_decode($file, false);  //Takes a JSON encoded string and converts it
 <body>
     <title>hikeMe</title>
 <?php require_once'includes/navbar.php';?>
-<img id="banner" src="assets/img/photographer-1031393_1920.jpg" alt="Photographer Banner Image">
 
     <div class="container">
     	<div class="text-center" style="margin-top: 50px;">
 		    <h1>Plan your hike</h1>
-			<p>Choose what day do you want to plan</p>
+			<p>Choose your day:</p>
 			<br>
 			
 			<div class="row">
