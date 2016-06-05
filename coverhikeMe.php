25e371791a3a597f84cadacf3975a92c6000986c
nathangruber <grubernp@gmail.com>
2016-06-05 10:57:58 -0500

    styling cover button

--- coverhikeMe.php
+++ coverhikeMe.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-<!-- saved from url=(0040)http://getbootstrap.com/examples/cover/# -->
 <html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -18,6 +17,7 @@
 
     <!-- Custom styles for this template -->
     <link href="assets/css/cover.css" rel="stylesheet">
+    <link href="assets/css/styles.css" rel="stylesheet">
 
     <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
     <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
@@ -50,7 +50,7 @@
             <h1 class="cover-heading">hikeMe</h1>
             <p class="lead">Search, Plan, Remember, and Share your Hike</p>
             <p class="lead">
-              <a href="http://ec2-54-213-132-61.us-west-2.compute.amazonaws.com/hikeMe/index.php" class="btn btn-lg btn-default">enter hikeMe</a>
+              <a href="http://ec2-54-213-132-61.us-west-2.compute.amazonaws.com/hikeMe/index.php" class="btn btn-lg btn-stripe">enter hikeMe</a>
             </p>
           </div>
 
