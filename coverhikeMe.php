aa399c038e0ebc14526e29d6b427265117b0c590
nathangruber <grubernp@gmail.com>
2016-06-05 11:03:31 -0500

    revert button size

--- coverhikeMe.php
+++ coverhikeMe.php
@@ -49,8 +49,7 @@
             <h1 class="cover-heading">hikeMe</h1>
             <p class="lead">Search, Plan, Remember, and Share your Hike</p>
             <p class="lead">
-              <a href="http://ec2-54-213-132-61.us-west-2.compute.amazonaws.com/hikeMe/index.php" class="btn btn-stripe">enter hikeMe</a>
-            </p>
+            <a href="http://ec2-54-213-132-61.us-west-2.compute.amazonaws.com/hikeMe/index.php" class="btn btn-lg btn-stripe">enter hikeMe</a></p>
           </div>
 
           <div class="mastfoot">
@@ -64,14 +63,9 @@
       </div>
 
     </div>
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
     <script src="assets/js/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="assets/js/jquery.min.js"><\/script>')</script>
     <script src="assets/js/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
   
 
