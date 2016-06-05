d1a75eefbc9d12a67473cfc4549625518fe18df9
nathangruber <grubernp@gmail.com>
2016-06-05 11:46:37 -0500

    edited text

--- coverhikeMe.php
+++ coverhikeMe.php
@@ -39,8 +39,9 @@
 
           <div class="masthead clearfix">
             <div class="inner">
-              <h3 class="masthead-brand"><small>I took a walk in the woods and come out taller than the trees.<br> - Henry David Thoreau</small></h3>
+              <h3 class="masthead-brand"></h3>
               <nav>
+                <p><small>I took a walk in the woods and come out taller than the trees.<br> - Henry David Thoreau</small><p>
               </nav>
             </div>
           </div>
