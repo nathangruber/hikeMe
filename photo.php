07cfa056d5675fccfff761c5c2fe466a7dd746d5
nathangruber <grubernp@gmail.com>
2016-06-07 16:55:49 -0500

    debug

--- photo.php
+++ photo.php
@@ -41,7 +41,7 @@ if(isset($_GET['option'])&&($_GET['option']=='setasprivate')){
 					
 					<img src="uploads/"<?php echo $result['name']; ?>" width=500px"/>
 					<?php
-						echo $result['name'];
+						echo print_r($result);
 						if($result['public']==0){
 					?>
 						
