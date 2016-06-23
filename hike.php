0e2d878d3dc37535343fc6bceacb8761ca7c1604
nathangruber <grubernp@gmail.com>
2016-06-22 22:01:05 -0500

    text edit

--- hike.php
+++ hike.php
@@ -67,7 +67,7 @@ if($hike_info['date']!="0000-00-00"){
     <?php } ?>
 	
 	<center><h1>Hike Editor<br>
-	    <small>Add a trail journal, upload photos, share your hike.</small></h1></center>	
+	    <small>Journal your Hike, Upload Photos, and Share</small></h1></center>	
 <!--<h1><?php echo $_GET['show'];?>&nbsp;Hikes</h1> -->		
   <?php
 	if(($logged)&&($ismyhike)){
