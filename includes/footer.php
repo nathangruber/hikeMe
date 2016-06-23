bf2a55877846f36bfcfb230021aa38ec08bfd7bb
nathangruber <grubernp@gmail.com>
2016-06-22 21:40:11 -0500

    link

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
 <div class="footer">
   <a href="https://www.facebook.com"><div class="social">&#62220;</div></a>
   <a href="https://twitter.com"><div class="social">&#62217;</div></a>
-  <a href"https://plus.google.com/"><div class="social">&#62223;</div></a>
+  <a href"https://plus.google.com/collections/featured"><div class="social">&#62223;</div></a>
   <a href="https://www.pinterest.com"><div class="social">&#62226;</div></a>
 </div>
   <center><p><small>&copy; 2016 hikeMe. Created by Nathan Gruber. All rights reserved. <a href="sitemap.php">Site Map</a></small></p></center>
