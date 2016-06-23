f8a63aa64040f98c4d916c89c6406afa7e77a81f
nathangruber <grubernp@gmail.com>
2016-06-22 22:51:45 -0500

    removed break

--- hikedone.php
+++ hikedone.php
@@ -71,7 +71,7 @@ $hike_info = $hike->getHikeInfo($hike_id,$_SESSION['id']);
 					  ?>
 					</div>
 					<div class="form-group">
-					<button class="btn btn-success " name="submit" type="submit">Submit</button><br>
+					<button class="btn btn-success " name="submit" type="submit">Submit</button>
 					</div>
 				</form>	
 				</div>
