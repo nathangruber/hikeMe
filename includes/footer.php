12733fa3fd334a2d080ac9030447c0cf378c086b
nathangruber <grubernp@gmail.com>
2016-06-22 21:38:26 -0500

    footer done

--- includes/footer.php
+++ includes/footer.php
@@ -1,34 +1,11 @@
-  <div class="content">hikeMe</div>
-  <div class="footer">
+<div class="content">hikeMe</div>
+<div class="footer">
   <a href="https://www.facebook.com"><div class="social">&#62220;</div></a>
-    <div class="social">&#62217;</div>
-    <div class="social">&#62223;</div>
-    <div class="social">&#62226;</div>
-  </div>
+  <a href="https://twitter.com"><div class="social">&#62217;</div></a>
+  <a href"https://plus.google.com/"><div class="social">&#62223;</div></a>
+  <a href="https://www.pinterest.com"><div class="social">&#62226;</div></a>
+</div>
   <center><p><small>&copy; 2016 hikeMe. Created by Nathan Gruber. All rights reserved. <a href="sitemap.php">Site Map</a></small></p></center>
-  <script src="assets/js/jquery.min.js"></script>
-  <script src="assets/js/bootstrap.min.js"></script>
-
-<script>window.twttr = (function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0],
-    t = window.twttr || {};
-  if (d.getElementById(id)) return t;
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "https://platform.twitter.com/widgets.js";
-  fjs.parentNode.insertBefore(js, fjs);
- 
-  t._e = [];
-  t.ready = function(f) {
-    t._e.push(f);
-  };
- 
-  return t;
-}(document, "script", "twitter-wjs"));</script>
-
-
-
-
-
-<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-576b116195616225"></script>
