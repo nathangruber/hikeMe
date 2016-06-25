04e3a5fd07084fb01ffb1d50402324b0c0df64b8
nathangruber <grubernp@gmail.com>
2016-06-24 21:48:51 -0500

    added tag

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,9 @@
 <div class="content">hikeMe</div>
 <div class="footer">
-  <a href="https://www.facebook.com"><div class="social">&#62220;</div></a>
-  <a href="https://twitter.com"><div class="social-1">&#62217;</div></a>
-  <a href="https://plus.google.com/collections/featured"><div class="social-2">&#62223;</div></a>
-  <a href="https://www.pinterest.com"><div class="social-3">&#62226;</div></a>
+  <a href="https://www.facebook.com"><a><div class="social">&#62220;</div></a>
+  <a href="https://twitter.com"><a><div class="social-1">&#62217;</div></a>
+  <a href="https://plus.google.com/collections/featured"><a><div class="social-2">&#62223;</div></a>
+  <a href="https://www.pinterest.com"><a><div class="social-3">&#62226;</div></a>
 </div>
   <center><p><small>&copy; 2016 hikeMe. Created by Nathan Gruber. All rights reserved. <a href="sitemap.php">Site Map</a></small></p></center>
 <script src="assets/js/jquery.min.js"></script>
