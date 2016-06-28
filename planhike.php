85d75e14eff652194477cdca662b52ac007268e0
nathangruber <grubernp@gmail.com>
2016-06-28 15:35:22 -0500

    debug file

--- planhike.php
+++ planhike.php
@@ -66,6 +66,8 @@ $file = file_get_contents($url, false, $context);  //read the contents of a file
 
 $obj = json_decode($file, false);  //Takes a JSON encoded string and converts it into a PHP variable.
 
+echo $file;
+
 ?>
 
 <!DOCTYPE html>
